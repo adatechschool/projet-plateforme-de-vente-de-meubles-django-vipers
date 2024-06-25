83a4d39fb39682f9242323565e34e22835b6b5ee
oum4512 <159126137+oum4512@users.noreply.github.com>
2024-06-25 13:55:50 +0200

    Search_bar

---
+++ front_end/src/components/atoms/Search_bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search_bar = () => {
+  return (
+    <div>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+      
+    </div>
+  )
+}
+
+export default Search_bar
