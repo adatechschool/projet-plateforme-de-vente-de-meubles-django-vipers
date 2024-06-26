91ecef279edc0ada5f2e65c1e1d8101c64ad6350
AramirRp <antoinerp@gmail.com>
2024-06-26 14:13:25 +0200

    footer_center

--- front_end/src/components/molecules/Footer_center.jsx
+++ front_end/src/components/molecules/Footer_center.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Social_medias from "../../components/atoms/Social_medias.jsx";
+import Contact_us from "../atoms/Contact_us.jsx";
+
 
 const Footer_center = () => {
   return (
@@ -7,6 +9,8 @@ const Footer_center = () => {
       <h1 className="text-4xl font-bold">Follow Us</h1>
 
       <Social_medias />
+      <h1 className="text-4xl font-bold mb-4 mt-4">Nous contacter</h1>
+      <Contact_us />
     </div>
   );
 };
