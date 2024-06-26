23e265f73d26517b3911f2262d8b3b99ba647114
Pauline Taillemite <paulinetaillemite@MacBook-Air-de-Pauline.local>
2024-06-26 14:21:32 +0200

    les logos fav, panier et account

--- front_end/src/components/molecules/Navbar.jsx
+++ front_end/src/components/molecules/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Menu_burger from '../atoms/Menu_burger'
 import Search_bar from '../atoms/Search_bar'
+import Account_logos from '../atoms/Account_logos'
 
 const Navbar = () => {
   return (
-    <div>
-      <Search_bar/>
+    <div className='flex'>
       <Menu_burger/>
+      <Search_bar/>
+      <Account_logos />
     </div>
   )
 }
