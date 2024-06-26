4afbbcabd136305a36bcd0c8b74fb93da0936fc6
Terrassier Thimothée <thimotheeterrassier@gmail.com>
2024-06-26 10:58:34 +0200

    creation compte admin

--- back_end/django_vipers_api/furniture/admin.py
+++ back_end/django_vipers_api/furniture/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from furniture.models import Furniture, Type, Color, Material
 
 # Register your models here.
+
+admin.site.register(Furniture)
+admin.site.register(Type)
+admin.site.register(Color)
+admin.site.register(Material)
