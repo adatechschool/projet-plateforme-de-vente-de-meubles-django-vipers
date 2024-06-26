cee446788a25c27390b5a0f4221bae9f2fef6b85
Blanchou81 <bakakacademic@yahoo.fr>
2024-06-26 10:37:02 +0200

    social_medias page

--- front_end/src/components/atoms/Social_medias.jsx
+++ front_end/src/components/atoms/Social_medias.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import facebookLogo from '../../assets/facebook (3).png';
+import twitterLogo from '../../assets/twitter.png';
+import instagramLogo from '../../assets/instagram.png';
+import linkedinLogo from '../../assets/linkedin.png';
+import tictacLogo from '../../assets/tic-tac.png';
+
+
 
 const Social_medias = () => {
+
   return (
-    <div>Social_medias</div>
-  )
-}
+  <>
+
+    <ul>
+
+      <li><a href="https://www.facebook.com"><img src={facebookLogo} alt="" /></a></li>
+      <li><a href="https://www.twitter.com"><img src={twitterLogo} alt="" /></a></li>
+      <li><a href="https://www.instagram.com"><img src={instagramLogo} alt="" /></a></li>
+      <li><a href="https://www.linkedin.com"><img src={linkedinLogo} alt="" /></a></li>
+      <li><a href="https://www.tictac.com"><img src={tictacLogo} alt="" /></a></li>
+
+    </ul>
+    
+  </>
+);
+};
 
 export default Social_medias
+
+
+
