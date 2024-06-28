0460edb1b236f58a6c25a7d0bc66c42e8564cb8d
Pauline Taillemite <paulinetaillemite@MacBook-Air-de-Pauline.local>
2024-06-28 11:17:27 +0200

    maj catégories

--- front_end/src/components/organisms/Categories.jsx
+++ front_end/src/components/organisms/Categories.jsx
@@ -8,26 +8,35 @@ import objects from '../../assets/objects.jpg'
 const Categories = () => {
     return (
         <>
-            <div className='flex space-x-4'>
+
+
+            <h1 className=' pl-24 pb-2 text-8xl font-light text-left lowercase'>categories.</h1>
+            <div className='relative grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pl-20 pr-20 pt-2 pb-10 text-white font-normal'>
+
+
+
                 <div className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
                     <img className='object-cover z-1 h-full w-full opacity-75' src={chairs} />
-                    <a href='#' className='absolute top-1/2 w-full z-10 text-4xl text-center'>chairs & sofas</a>
+                    <a href='#' className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-10 text-4xl text-center'>chairs & sofas</a>
                 </div>
 
-                <a href='#' className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
-                    <img className='object-cover h-full w-full opacity-75' src={lamps} />
-                    <a href='#' className='absolute top-1/2 bott/2 w-full z-10 text-4xl text-center'>lamps</a>
-                </a>
+                <div className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
+                    <img className='object-cover z-1 h-full w-full opacity-75' src={lamps} />
+                    <a href='#' className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-10 text-4xl text-center'>lamps</a>
+                </div>
 
-                <a href='#' className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
-                    <img className='object-cover h-full w-full opacity-75' src={tables} />
-                    <a href='#' className='absolute top-1/2 bott/2 w-full z-10 text-4xl text-center'>tables</a>
-                </a>
+                <div className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
+                    <img className='object-cover z-1 h-full w-full opacity-75' src={tables} />
+                    <a href='#' className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-10 text-4xl text-center'>tables</a>
+                </div>
 
-                <a href='#' className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
-                    <img className='object-cover h-full w-full opacity-75' src={objects} />
-                    <a href='#' className='absolute top-1/2 bott/2 w-full z-10 text-4xl text-center'>objects</a>
-                </a>
+                <div className="w-auto h-auto basis-1/4 rounded-full overflow-hidden aspect-square relative">
+                    <img className='object-cover z-1 h-full w-full opacity-75' src={objects} />
+                    <a href='#' className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-10 text-4xl text-center'>objects</a>
+                </div>
+
+
+
             </div>
         </>
     )
