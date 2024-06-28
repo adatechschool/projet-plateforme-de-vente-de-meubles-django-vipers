998f98811453f1f0b5b445d08d9ee9e2f9183599
Pauline Taillemite <paulinetaillemite@MacBook-Air-de-Pauline.local>
2024-06-28 11:54:47 +0200

    titre new arrivals responsive

--- front_end/src/components/templates/Arrivals.jsx
+++ front_end/src/components/templates/Arrivals.jsx
@@ -3,10 +3,11 @@ import Product_grid from '../organisms/Product_grid'
 
 const Arrivals = () => {
   return (
-    <div className="pt-16 pb-16 relative">
-    <h1 className="z-10 text-8xl font-extralight text-right uppercase absolute top-6 right-6">new arrivals</h1>
-    <Product_grid className="z-9" />
-  </div>
+<div className="pt-16 pb-16 relative">
+  <h1 className="z-10 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-extralight text-right uppercase absolute top-6 right-6">new arrivals</h1>
+  <Product_grid className="z-9" />
+</div>
+
   
   )
 }
