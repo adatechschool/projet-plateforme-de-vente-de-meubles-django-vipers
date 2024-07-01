72ccbbfc278ac888c4b16c01661181b258df6b6a
Terrassier Thimothée <thimotheeterrassier@gmail.com>
2024-07-01 11:38:54 +0200

    fetch api

--- front_end/src/components/molecules/Product_card.jsx
+++ front_end/src/components/molecules/Product_card.jsx
@@ -5,14 +5,14 @@ import Product_details from "../atoms/Product_details";
 import Product_photo from "../atoms/Product_photo";
 
 
-const Product_card = ({
+const Product_card = ({ 
    
 }) => {
   return (
     <div>
       <div className="relative group box-content w-64 h-96">
         <figure className="h-full transition duration-500 ease-in-out group-hover:blur-md">
-          <Product_photo className=""  />
+          <Product_photo className="" />
         </figure>
         <div className="absolute inset-0 flex flex-col items-center opacity-0 group-hover:opacity-100">
           <div className="card-body items-center">
--- front_end/src/components/pages/Home.jsx
+++ front_end/src/components/pages/Home.jsx
@@ -1,18 +1,35 @@
-import Hero from '../organisms/Hero'
-import Recommandations from '../templates/Recommandations'
-import Arrivals from '../templates/Arrivals'
-import Categories from '../organisms/Categories'
-
+import Hero from "../organisms/Hero";
+import Recommandations from "../templates/Recommandations";
+import Arrivals from "../templates/Arrivals";
+import Categories from "../organisms/Categories";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [arrivals, setArrivals] = useState([]);
+  useEffect(() => {
+    try {
+      async function fetchFurniture() {
+        const furnitureData = await fetch(
+          "http://127.0.0.1:8000/api/furniture"
+        );
+        const parseFurnitureData = await furnitureData.json();
+        setArrivals(parseFurnitureData);
+        console.log(parseFurnitureData);
+      }
+      fetchFurniture();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div>
-        <Hero />
-        <Recommandations />
-        <Arrivals />
-        <Categories />
+      <Hero />
+      <Recommandations />
+      <Arrivals arrivals = {arrivals} />
+      <Categories />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
