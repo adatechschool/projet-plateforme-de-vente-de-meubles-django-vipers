05079fee9fcf77f6067986b73c67b2df284a7bdd
Terrassier Thimothée <thimotheeterrassier@gmail.com>
2024-07-02 14:08:04 +0200

    ajout page product, detail

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,38 +1,43 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/molecules/Navbar";
 import Home from "./components/pages/Home";
 import Footer from "./components/organisms/Footer";
 import Connexion from "./components/pages/Connexion";
-import LoginForm from "./components/atoms/LoginForm"
+import LoginForm from "./components/atoms/LoginForm";
 import SignupForm from "./components/atoms/SignupForm";
+import Product from "./components/pages/Product";
 
 function App() {
-    return (
-        <>
-            <div className="flex flex-col min-h-screen">
-                <Navbar />
 
-                <main className="grow">
-                  {/* Router qui mène vers les diffèrentes pages, NavBar et Foooter ne change pas */}
-                    <Router>
-                        <Routes>
-                            {/* URL vers Home */}
-                            <Route path="/" element={<Home />} />
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
 
-                            {/* URL vers page de connexion, soit Login, soit Signup */}
-                            <Route path="/connexion" element={<Connexion />}>
-                                <Route path="login" element={<LoginForm />} />
-                                <Route path="signup" element={<SignupForm />} />
-                            </Route>
-                        </Routes>
-                    </Router>
-                </main>
+        <main className="grow">
+          {/* Router qui mène vers les diffèrentes pages, NavBar et Foooter ne change pas */}
+          <Router>
+            <Routes>
+              {/* URL vers Home */}
+              <Route path="/" element={<Home />} />
 
-                <Footer />
-            </div>
-        </>
-    );
+              {/* URL vers page de connexion, soit Login, soit Signup */}
+              <Route path="/connexion" element={<Connexion />}>
+                <Route path="login" element={<LoginForm />} />
+                <Route path="signup" element={<SignupForm />} />
+              </Route>
+
+              <Route path="/product" element={<Product />}/>
+            
+            
+            </Routes>
+          </Router>
+        </main>
+
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- front_end/src/components/atoms/Product_details.jsx
+++ front_end/src/components/atoms/Product_details.jsx
@@ -1,38 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Product_details = ({ product }) => {
   return (
     <div>
-        <div className=" mt-3 ml-3 flex flex-col gap-[60px]">
-      <div className="text-center">
-        <h2 className="text-2xl font-bold text-black">{product.name}</h2>
-        <div className="mt-2 mb-2 flex space-x-2 items-center">
-        <div className="">
-            {product.dimensions}
+      <div className=" mt-3 ml-3 flex flex-col gap-[60px]">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-black">{product.name}</h2>
+          <div>
+            <img src={product.image1} alt={product.name}  className="object-cover rounded-box w-[30%]"/>
+            <img src={product.image2} alt={product.name}  className="object-cover rounded-box w-[30%]" />
           </div>
-          <div className="">
-            {product.description}
+          <div className="mt-2 mb-2 flex space-x-2 items-center">
+            <div className="">{product.dimensions}</div>
+            <div className="">{product.description}</div>
+            <div className="">{product.available}</div>
+            <div className="">{product.types}</div>
+            <div className="">{product.colors}</div>
           </div>
-          <div className="">
-            {product.available}
-          </div>
-          <div className="">
-            {product.types}
-          </div>
-          <div className="">
-            {product.colors}
-          </div>
-        </div>
 
-        <div className="mt-4 text-gray-600">
-          </div>
+          <div className="mt-4 text-gray-600"></div>
           <span className="font-bold text-black">{product.materials}</span>
-    
+        </div>
       </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Product_details
+export default Product_details;
