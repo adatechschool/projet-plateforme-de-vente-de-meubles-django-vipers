da9d9776bbee1ccdf1a65be55ea6b77de0897ad6
Z0uLf4T <A.Zoulfat@gmail.com>
2024-07-02 14:50:37 +0200

    url get request color & material

--- back_end/django_vipers_api/furniture/serializers.py
+++ back_end/django_vipers_api/furniture/serializers.py
@@ -2,7 +2,8 @@
 from rest_framework import serializers
 from .models import Furniture
 from .models import Type
-
+from .models import Material
+from .models import Color
 # pour convertir le django en Json (on va convertir Furniture)
 class FurnitureSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,4 +13,14 @@ class FurnitureSerializer(serializers.ModelSerializer):
 class TypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Type
+        fields = '__all__' 
+
+class MaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = '__all__' 
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
         fields = '__all__' 
--- back_end/django_vipers_api/furniture/views.py
+++ back_end/django_vipers_api/furniture/views.py
@@ -5,8 +5,12 @@ from rest_framework import viewsets
 
 from .models import Furniture
 from .models import Type
+from .models import Material
+from .models import Color
 from .serializers import FurnitureSerializer
 from .serializers import TypeSerializer
+from .serializers import MaterialSerializer
+from .serializers import ColorSerializer
 
 
 class FurnitureViewSet(viewsets.ModelViewSet):
@@ -16,3 +20,11 @@ class FurnitureViewSet(viewsets.ModelViewSet):
 class TypeViewSet(viewsets.ModelViewSet):
     serializer_class = TypeSerializer
     queryset = Type.objects.all()
+
+class MaterialViewSet(viewsets.ModelViewSet):
+    serializer_class = MaterialSerializer
+    queryset = Material.objects.all()
+
+class ColorViewSet(viewsets.ModelViewSet):
+    serializer_class = ColorSerializer
+    queryset = Color.objects.all()
