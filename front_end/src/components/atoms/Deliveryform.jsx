6d51601cb0c28489a510560a353f521e57b604cc
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-07-03 16:52:02 +0200

    Section Delivery Done

--- front_end/src/components/atoms/Deliveryform.jsx
+++ front_end/src/components/atoms/Deliveryform.jsx
@@ -8,37 +8,65 @@ const Deliveryform = () => {
             <h1 class="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Delivery Address</h1>
             <br />
 
-         {/* SECTION NAME */}
-        <label class="block mb-6">
-            <span class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</span>
-            <input name="name" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
-          placeholder="Enter your name"/>
-        </label>
-
         {/* SECTION ADDRESS */}
         <div class="mb-4">
           <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address 1</label>
-          <input type="text" id="address-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          <input type="text" id="address-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='' />
         </div>
 
         <div class="mb-5">
           <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address 2</label>
-          <input type="text" id="address-2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          <input type="text" id="address-2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='' />
         </div>
 
         {/* SECTION CITY */}
         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
         <input
-          type="text" id="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
-        />
+          type="text" id="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='' />
         <br />
+
         {/* SECTION PROVINCE */}
         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Province</label>
         <input type="text" id="province" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=''
         />
 
+        <br />
+         {/* SECTION POSTAL CODE */}
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postal Code</label>
+        <input type="text" id="postal-code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=''
+        />  
 
+        <br />
+         {/* SECTION COUNTRY */}
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+        <input type="text" id="country" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=''
+        />  
+
+        <br />
+         {/* SECTION TELEPHONE */}
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telephone</label>
+        <input type="text" id="telephone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=''
+        />  
+
+        <br />
+        <div class="mb-6">
+            <button type="submit" class="
+            h-10
+            px-5
+            text-indigo-100
+            bg-indigo-700
+            rounded-lg
+            transition-colors
+            duration-150
+            focus:shadow-outline
+            hover:bg-indigo-800
+          "
+        >Save</button>
+        </div>
       </form>
+
+        
+    
     );
   }
   
--- front_end/src/components/pages/Shipping.jsx
+++ front_end/src/components/pages/Shipping.jsx
@@ -3,7 +3,7 @@ import Deliveryform from '../atoms/Deliveryform'
 
 const Shipping = () => {
     return (
-<div><h1>Delivery Address</h1>
+<div>
 <Deliveryform /> {/* Inclure le composant Deliveryform ici */}
 </div>
 
