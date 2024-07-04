964f08f34a58edba6b6a71588fafb04054aa6346
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-07-04 11:10:49 +0200

    shipping page finished

--- front_end/src/components/atoms/Deliveryform.jsx
+++ front_end/src/components/atoms/Deliveryform.jsx
@@ -5,7 +5,7 @@ const Deliveryform = () => {
 
         <form class="max-w-sm mx-auto">
 
-            <h1 class="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Delivery Address</h1>
+            <h1 class="font-mono block mb-2 text-xl font-medium text-gray-900 dark:text-white">Delivery Address</h1>
             <br />
 
         {/* SECTION ADDRESS */}
@@ -48,25 +48,29 @@ const Deliveryform = () => {
         <input type="text" id="telephone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=''
         />  
 
-        <br />
-        <div class="mb-6">
-            <button type="submit" class="
-            h-10
-            px-5
-            text-indigo-100
-            bg-indigo-700
-            rounded-lg
-            transition-colors
-            duration-150
-            focus:shadow-outline
-            hover:bg-indigo-800
-          "
-        >Save</button>
-        </div>
-      </form>
+        {/* SECTION BOUTONS SAVE ET NEXT */}
 
-        
-    
+        <br />
+        <div className="flex justify-between space-x-1 mb-6">
+        <a href="#_" class="relative inline-block text-sm group">
+        <span class="relative z-10 block px-5 py-3 overflow-hidden font-bold leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+        <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+        <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#beaa7c] group-hover:-rotate-180 ease"></span>
+        <span class="relative">Save</span>
+          </span>
+        <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#beaa7c] rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+        </a>
+
+        <a href="#_" class="relative inline-block text-sm group">
+        <span class="relative z-10 block px-5 py-3 overflow-hidden font-bold leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+        <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+        <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#beaa7c] group-hover:-rotate-180 ease"></span>
+        <span class="relative">Next</span>
+          </span>
+        <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#beaa7c] rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+        </a>
+      </div>
+      </form>
     );
   }
   
